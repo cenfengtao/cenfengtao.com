90bd1b0c8c59433ac0350f4a59a7c0603e7c54d0
cenfengtao <1806152243@qq.com>
2018-04-15 23:09:58 +0800

    优培圈
    优培圈

--- youpei_2.0/Application/Admin/Model/BaseModel.class.php
+++
@@ -1,8 +0,0 @@
-<?php
- namespace Admin\Model;
- use Common\Model\BaseModel as Base;
-
-class BaseModel extends Base {
-   
-};
-?>
--- youpei_2.0/Application/Home/Model/BaseModel.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Home\Model;
-use Common\Model\BaseModel as Base;
-
-class BaseModel extends Base {
-   
-};
-?>
